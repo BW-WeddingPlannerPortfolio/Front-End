051bebeae7f7c0a55105eeb4ec2a47b736253859
VodeniZeko <salexy.es@gmail.com>
2020-02-29 00:33:07 -0800

    create react app *install dependecies *add few util folder *create image upload

---
+++ client/src/components/Cloudinary.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { CloudinaryContext, Image, Transformation } from "cloudinary-react";
+import { fetchPhotos, openUploadWidget } from "../util/CloudinaryService";
+
+export const Cloudinary = () => {
+  const [images, setImages] = useState([]);
+
+  const beginUpload = tag => {
+    const uploadOptions = {
+      cloudName: "takija",
+      tags: [tag, "anImage"],
+      uploadPreset: "upload"
+    };
+    openUploadWidget(uploadOptions, (error, photos) => {
+      if (!error) {
+        console.log(photos);
+        if (photos.event === "success") {
+          setImages([...images, photos.info.public_id]);
+        }
+      } else {
+        console.log(error);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchPhotos("image", setImages);
+  }, []);
+  console.log(images);
+  return (
+    <CloudinaryContext cloudName="takija">
+      <div>
+        <button onClick={() => beginUpload("image")}>Upload Image</button>
+        <div>
+          {images.map(i => (
+            <Image publicId={i} key={i}>
+              <Transformation width="150" height="150" crop="fill" />
+            </Image>
+          ))}
+        </div>
+      </div>
+    </CloudinaryContext>
+  );
+};
