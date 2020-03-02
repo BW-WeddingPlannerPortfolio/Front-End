e25f68cc2012f6bc9c2f988eb1ead6cdfcc9eaa4
VodeniZeko <salexy.es@gmail.com>
2020-03-02 13:40:17 -0800

    redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,27 @@
-// import {
-// // ACTIONS//
-// } from "../actions/";
+import { DATA_START, DATA_SUCCESS, DATA_FAILURE } from "../actions";
 
 const initialState = {
   loading: false
 };
 
 export const reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case DATA_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case DATA_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+    case DATA_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+  }
 };
